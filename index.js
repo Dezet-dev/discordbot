21fc13302bfb2c99d76d75525ca4fae7fe2cc060
Dezet <51858024+Dezet-dev@users.noreply.github.com>
2019-12-07 21:52:22 +0100

    Poradnik #2
    Komenda say oraz embed

--- index.js
+++ index.js
@@ -9,6 +9,35 @@ bot.on("ready", async () => {
     console.log(`${nazwabota} jest online`)
 });
 
+bot.on("message", async message => {
+    if (message.author.bot) return;
+ 
+    if (message.content.indexOf(prefix) !== 0) return;
+    var args = message.content.slice(prefix.length).trim().split(/ +/g);
+    var command = args.shift().toLowerCase()
+
+
+if(command == "say"){
+    message.delete() 
+   
+ 
+
+   if(message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(message.content.slice(prefix.length+3)) 
+   else
+   return message.channel.send("Nie posiadasz permisji!")  
+}
+
+if(command == "embed"){
+    var embed = new Discord.RichEmbed()
+    .setDescription("Poradnik Embedy Discord.js")
+    .setTitle("Embed info")
+    .setColor(`#ff0000`)
+    .setThumbnail(bot.user.avatarURL)
+    
+    message.channel.send(embed)
+
+}
+});     
 
 
 bot.login(botconfig.token)
